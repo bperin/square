772259633e83e6fb28fffcadb4eb0c9aa24ac078
Brian <bperin42@gmail.com>
2021-04-23 01:54:21 -0700

    cleanup

--- app/src/main/java/com/brianperin/squaredirectory/DirectoryApp.kt
+++ app/src/main/java/com/brianperin/squaredirectory/DirectoryApp.kt
@@ -1,19 +1,12 @@
 package com.brianperin.squaredirectory
 
 import android.app.Application
-import com.facebook.cache.common.CacheKey
-import com.facebook.cache.disk.DiskCacheConfig
-import com.facebook.drawee.backends.pipeline.Fresco
-import com.facebook.imagepipeline.cache.ImageCacheStatsTracker
-import com.facebook.imagepipeline.cache.MemoryCache
-import com.facebook.imagepipeline.core.ImagePipelineConfig
 import timber.log.Timber
 
 class DirectoryApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-        Fresco.initialize(this, ImagePipelineConfig.newBuilder(this).setDiskCacheEnabled(true).build())
     }
 
 }
