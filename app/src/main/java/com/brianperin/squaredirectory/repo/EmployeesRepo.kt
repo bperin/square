12aa10bfb1c7dd2e1ce94c20501bb5a543919027
Brian <bperin42@gmail.com>
2021-04-22 21:37:00 -0700

    doesn't do a lot but sets us up if we want to cache alater

--- app/src/main/java/com/brianperin/squaredirectory/repo/EmployeesRepo.kt
+++ app/src/main/java/com/brianperin/squaredirectory/repo/EmployeesRepo.kt
@@ -1,13 +1,27 @@
 package com.brianperin.squaredirectory.repo
 
+import com.brianperin.squaredirectory.model.response.Employees
 import com.brianperin.squaredirectory.network.ApiClient
 import com.brianperin.squaredirectory.network.BaseDataSource
+import com.brianperin.squaredirectory.network.Result
+
 
 /**
  * Repository layer for fetching list of employees
  * no caching network calls
+ * here we could add a cache if we wanted
  */
 class EmployeesRepo() : BaseDataSource() {
 
-    suspend fun getEmployees() = getResult { ApiClient.apiService.getEmployees() }
+    private var employeesResult: Result<Employees>? = null
+
+    suspend fun getEmployees(): Result<Employees> {
+        return if (employeesResult == null) {
+            employeesResult = getResult { ApiClient.apiService.getEmployees() }
+            employeesResult as Result<Employees>
+        } else {
+            employeesResult as Result<Employees>
+        }
+
+    }
 }
